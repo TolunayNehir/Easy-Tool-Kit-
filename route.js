004d04830c1799044a4b66c317c8ac66262b274c
TolunayNehir <103363053+TolunayNehir@users.noreply.github.com>
2023-06-16 15:58:04 +0300

    Update route.js

--- route.js
+++ route.js
@@ -67,6 +67,98 @@ router.get('/',(req,res)=>{
   }
  });
 
+ router.get('/insert/:key/:datas/:values',(req,res)=>{
+  key=req.params['key']
+  if(key==process.env.key) {
+  values=req.params['values']
+  datas=req.params['datas']
+  arr=values.split("*");
+  arr2=datas.split("*");
+  let str;
+  let str2;
+  var i1=arr.length;
+  var i2=arr2.length;
+  let tablename=req.query.table;
+  arr2.forEach(element=>{
+      if(i2!=1){
+         str2+=element+","
+      }
+      else{
+        str2+=element
+      }
+    i2--;
+  });
+  arr.forEach(element => {
+    if(parseInt(element)/1==parseInt(element)){
+      if(i1!=1){
+         str+=element+","
+      }
+      else{
+        str+=element
+      }
+    }
+    else{
+      if(i1!=1){
+        str+="'"+element+"',"
+     }
+     else{
+       str+=element
+     }
+    }
+    i1--;
+  });
+  let sql = "INSERT INTO "+process.env.database+"."+tablename+" ( "+str2+") VALUES ( "+str+");";
+  con.query(sql,function (err, result) {
+    if (err)  console.log(err);
+    res.send(result);
+    res.end();
+  });
+  }
+  else{
+    res.send("You are not authorized");
+    res.end();
+  }
+ });
+
+ router.get('/deletebyid/:key/:id',(req,res)=>{
+  key=req.params['key']
+  if(key==process.env.key) {
+    id=req.params['id']
+    let tablename=req.query.table;
+  let sql = "delete from "+process.env.database+"."+tablename+" where id="+id;
+  con.query(sql,function (err, result) {
+    if (err)  console.log(err);
+    res.send(result);
+    res.end();
+  });
+}
+  else{
+    res.send("You are not authorized");
+    res.end();
+  }
+ });
+
+
+router.get('/deletebytag/:key/:tag/:data',(req,res)=>{
+    key=req.params['key']
+    if(key==process.env.key) {
+      tag=req.params['tag']
+      data=req.params['data']
+      let tablename=req.query.table;
+    let sql = "delete from "+process.env.database+"."+tablename+" where "+tag+"="+data;
+    con.query(sql,function (err, result) {
+      if (err)  console.log(err);
+      res.send(result);
+      res.end();
+    });
+  }
+
+  else{
+    res.send("You are not authorized");
+    res.end();
+  }
+ });
+
 var con = mysql.createConnection({
     host: process.env.host,
     user: process.env.user,
